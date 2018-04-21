015e991a8e02135290ea71a285fe50356ed4b270
foreverbell <dql.foreverbell@gmail.com>
2018-04-21 13:36:20 +0800

    mm: use new PDE and PTEs.

--- src/lib.rs
+++ src/lib.rs
@@ -27,23 +27,17 @@ use krnl::gdt;
 use krnl::idt;
 
 fn test() {
-  let hello = b"Hello World!";
-  let color_byte = 0x1f; // white foreground, blue background
-
-  let mut hello_colored = [color_byte; 24];
-  for (i, char_byte) in hello.into_iter().enumerate() {
-    hello_colored[i * 2] = *char_byte;
-  }
-
-  // write `Hello World!` to the center of the VGA text buffer
-  let buffer_ptr = 0xb8000 as *mut _;
-  unsafe { *buffer_ptr = hello_colored };
-
-  console::CONSOLE.lock().putch(b'\n');
-  console::CONSOLE.lock().putch(b'a');
-
-  printf!(" {:?}\n", Some(666));
-
+  console::CONSOLE.lock().setcolor(
+    console::Color::White,
+    console::Color::Blue,
+    false,
+  );
+  printf!("Hello World!\n");
+  console::CONSOLE.lock().setcolor(
+    console::Color::LightGrey,
+    console::Color::Black,
+    false,
+  );
   printf!("{}\n", krnl::sys_time::get());
 }
 
--- src/mm/init.rs
+++ src/mm/init.rs
@@ -2,26 +2,6 @@ use core::ptr::Unique;
 use krnl::cmos;
 use volatile::Volatile;
 
-bitflags! {
-  struct EntryFlags: u32 {
-    const PRESENT = 1 << 0;
-    const WRITE = 1 << 1;
-  }
-}
-
-type PageTable = Unique<[Volatile<u32>; 1024]>;
-
-const PDE: PageTable = unsafe { Unique::new_unchecked(0x1000 as *mut _) };
-
-const PTE: [PageTable; 4] = unsafe {
-  [
-    Unique::new_unchecked(0x2000 as *mut _),
-    Unique::new_unchecked(0x3000 as *mut _),
-    Unique::new_unchecked(0x4000 as *mut _),
-    Unique::new_unchecked(0x5000 as *mut _),
-  ]
-};
-
 const NVRAM_BASELO: u8 = 0x15;
 const NVRAM_BASEHI: u8 = 0x16;
 const NVRAM_EXTLO: u8 = 0x17;
@@ -56,6 +36,68 @@ fn detect() {
   );
 }
 
+bitflags! {
+  struct PageEntryFlags: u32 {
+    const PRESENT = 1 << 0;
+    const WRITE = 1 << 1;
+  }
+}
+
+const TABLE_ENTRIES: usize = 1024;
+type PageTable = Unique<[Volatile<u32>; TABLE_ENTRIES]>;
+
 pub fn initialize() {
   detect();
+
+  const PDE: PageTable = unsafe { Unique::new_unchecked(0x1000 as *mut _) };
+  const PTES: [PageTable; 4] = unsafe {
+    [
+      Unique::new_unchecked(0x2000 as *mut _),
+      Unique::new_unchecked(0x3000 as *mut _),
+      Unique::new_unchecked(0x4000 as *mut _),
+      Unique::new_unchecked(0x5000 as *mut _),
+    ]
+  };
+  let ENTRY_FLAGS: u32 =
+    (PageEntryFlags::PRESENT | PageEntryFlags::WRITE).bits();
+
+  // clear all PDE entries.
+  for i in 0..TABLE_ENTRIES {
+    unsafe {
+      PDE.as_mut()[i].write(0);
+    }
+  }
+
+  let setup = move |index: usize,
+                    PTE: &mut PageTable,
+                    phys: u32,
+                    from: usize,
+                    to: usize| {
+    unsafe {
+      PDE.as_mut()[index].write((PTE.as_ptr() as u32) | ENTRY_FLAGS);
+
+      for i in 0..TABLE_ENTRIES {
+        PTE.as_mut()[i].write(0);
+        if i >= from && i < to {
+          PTE.as_mut()[i].write((((i as u32) << 12) + phys) | ENTRY_FLAGS);
+        }
+      }
+    }
+  };
+
+  // set lower 1M memory as identity paging.
+  setup(0, &mut PTES[0], 0x0, 0, 256);
+
+  // 0xc0000000 ~ 0xc00f0000 for kernel (physical 0x0 ~ 0xf0000), 960K.
+  setup(768, &mut PTES[1], 0x0, 0, 240);
+
+  // leave a memory hole here as guard pages.
+
+  // 0xc0400000 ~ 0xc0c00000 for memory pool (physical 0x100000 ~ 0x900000), 8M.
+  setup(769, &mut PTES[2], 0x100000, 0, 1024);
+  setup(770, &mut PTES[3], 0x500000, 0, 1024);
+
+  unsafe {
+    asm!("mov $0, %cr3" :: "r" (PDE.as_ptr() as u32) : "memory" : "volatile");
+  }
 }
